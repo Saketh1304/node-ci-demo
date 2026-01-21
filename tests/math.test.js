dd7577eeee77e639d7d9990eb7b1f3a2a9a50f3a
Your Name <you@example.com>
2026-01-21 15:55:31 +0530

    Changed test case to check CI failure

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,7 @@
 const { add, subtract } = require('../src/math');
 
 test('adds two numbers', () => {
-  expect(add(2, 3)).toBe(5);
+  expect(add(2, 3)).toBe(6);
 });
 
 test('subtracts two numbers', () => {
